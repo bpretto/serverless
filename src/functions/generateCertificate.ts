a5e5af9f3717d3dcdeb922dc4e327dc3ec3d48ef
Bernardo Pretto <bernardodgpretto@gmail.com>
2021-07-07 16:54:39 -0300

    application deployed

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,11 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
 import { document } from "../utils/dynamodbClient";
 import chromium from "chrome-aws-lambda";
 import path from "path";
 import handlebars from "handlebars"
 import fs from "fs";
 import dayjs from "dayjs"
+import { S3 } from "aws-sdk"
 
 interface ICreateCertificate {
     id: string;
@@ -31,14 +33,26 @@ const compile = async function(data: ITemplate) {
 export const handle = async(event) => {
     const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate
 
-    await document.put({
+    const response = await document.query({
         TableName: "users_certificates",
-        Item: {
-            id,
-            name,
-            grade
+        KeyConditionExpression: "id = :id",
+        ExpressionAttributeValues: {
+            ":id": id
         }
     }).promise();
+
+    const userAlreadyExists = response.Items[0];
+
+    if(!userAlreadyExists) {
+        await document.put({
+            TableName: "users_certificates",
+            Item: {
+                id,
+                name,
+                grade
+            }
+        }).promise();
+    }
 
     const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
     const medal = fs.readFileSync(medalPath, "base64")
@@ -53,12 +67,42 @@ export const handle = async(event) => {
 
     const content = await compile(data);
 
-    
+    const browser = await chromium.puppeteer.launch({
+        headless: true,
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath
+    });
+
+    const page = await browser.newPage();
+
+    await page.setContent(content);
+
+    const pdf = await page.pdf({
+        format: "a4",
+        landscape: true,
+        path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+        printBackground: true,
+        preferCSSPageSize: true
+    }); 
+
+    await browser.close();
+
+    const s3 = new S3();
+
+    await s3.putObject({
+        Bucket: "igniteserverlesscertificates",
+        Key: `${id}.pdf`,
+        ACL: "public-read",
+        Body: pdf,
+        ContentType: "application/pdf"
+    }).promise();
 
     return {
         statusCode: 201,
         body: JSON.stringify({
-            message: "Certificate created!"
+            message: "Certificate created!",
+            url: `https://igniteserverlesscertificates.s3.sa-east-1.amazonaws.com/${id}.pdf`
         }),
         headers: {
             "Content-type": "application/json"
