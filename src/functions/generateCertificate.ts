edbfb8d7dd651a06a1257ac9a2394d924218f9ba
Bernardo Pretto <bernardodgpretto@gmail.com>
2021-07-07 14:10:19 -0300

    generating and compiling certificate

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,31 @@
-import { document } from "../utils/dynamodbClient"
+import { document } from "../utils/dynamodbClient";
+import chromium from "chrome-aws-lambda";
+import path from "path";
+import handlebars from "handlebars"
+import fs from "fs";
+import dayjs from "dayjs"
 
 interface ICreateCertificate {
     id: string;
     name: string;
     grade: string;
+}
+
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    date: string;
+    medal: string;
+}
+
+const compile = async function(data: ITemplate) {
+
+    const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+
+    const html = fs.readFileSync(filePath, "utf-8");
+
+    return handlebars.compile(html)(data);
 }
 
 export const handle = async(event) => {
@@ -17,6 +39,21 @@ export const handle = async(event) => {
             grade
         }
     }).promise();
+
+    const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
+    const medal = fs.readFileSync(medalPath, "base64")
+
+    const data: ITemplate = {
+        date: dayjs().format("DD/MM/YYYY"),
+        grade,
+        name,
+        id,
+        medal
+    }
+
+    const content = await compile(data);
+
+    
 
     return {
         statusCode: 201,
